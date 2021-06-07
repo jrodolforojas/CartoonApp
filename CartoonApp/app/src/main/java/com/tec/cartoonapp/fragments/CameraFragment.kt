6ede05622cc0b097c5af4b16a1a6833c239210c1
Rodolfo Rojas <jrodolforojasguzman@gmail.com>
2021-06-07 16:24:08 -0600

    Init Cartoon App project with Jetpack Compose

---
+++ CartoonApp/app/src/main/java/com/tec/cartoonapp/fragments/CameraFragment.kt
@@ -0,0 +1,24 @@
+package com.tec.cartoonapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tec.cartoonapp.R
+
+
+class CameraFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+}
